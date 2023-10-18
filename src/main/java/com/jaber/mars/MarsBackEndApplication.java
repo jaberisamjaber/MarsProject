265042623936198e515512f5e83c5c2b83fdd907
jaberisamjaber <jaberjaber018@gmail.com>
2023-10-18 15:44:53 -0400

    initial commit

---
+++ src/main/java/com/jaber/mars/MarsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.jaber.mars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaber/mars/MarsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaber.mars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
