514d422f3bf0a503aea754c3b0232fbcb8f060a4
jaberisamjaber <jaberjaber018@gmail.com>
2023-10-19 15:02:31 -0400

    initial set up for controller and service

---
+++ src/main/java/com/jaber/mars/controller/MarsController.java
@@ -0,0 +1,23 @@
+package com.jaber.mars.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jaber.mars.service.MarsService;
+
+@RestController
+public class MarsController {
+	
+	// autowiring the mars service
+	@Autowired
+	public MarsService marsService;
+	
+	// get request
+	@GetMapping("photos")
+	public String getMarsPhotos(@RequestParam String sol, @RequestParam String camera) {
+		return marsService.fetchPhotos(sol, camera);
+	}
+
+}
---
+++ src/main/java/com/jaber/mars/service/MarsService.java
@@ -0,0 +1,38 @@
+package com.jaber.mars.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@PropertySource("classpath:application.properties")
+public class MarsService {
+	
+	// injecting my key value
+	@Value("${mars.api.key}")
+	private String key;
+	
+	
+	// creating a rest template to call external api
+	private RestTemplate restTemplate = new RestTemplate();
+	
+	
+	// method to call external api
+	public String fetchPhotos(String sol, String camera) {
+		
+		// building the url
+		StringBuilder st = new StringBuilder("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?");
+		st.append("sol=" + sol);
+		st.append("&camera=" + camera);
+		st.append("&api_key=" + key);
+		
+		// calling the api
+		String response = restTemplate.getForObject(st.toString(), String.class);
+		// returning the response
+		return response;
+	}
+	
+
+}
